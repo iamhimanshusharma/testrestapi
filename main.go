70cc6ed03c0354ab7ac3c617f4464e433632b2b8
Himanshu Sharma <imyselfhimanshusharma@gmail.com>
2022-01-17 20:00:13 +0530

    test api for heroku

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome!")
+}
+
+func main() {
+
+	myrouter := mux.NewRouter().StrictSlash(true)
+	myrouter.HandleFunc("/", homePage).Methods("GET")
+
+	// port := os.Getenv("PORT")
+	log.Fatal(http.ListenAndServe(":3000", myrouter)) //for testing on local machine
+	// log.Fatal(http.ListenAndServe(":"+port, myrouter))
+}
